9fb98883fb6ee8444737c370600de1f461264012
Gael Rogelio Bejarano Dominguez <a.22308051281154@cbtis128.edu.mx>
2024-09-06 10:10:37 -0600

    Add files via upload
    Practica 1 de while y for

---
+++ mifor.py
@@ -0,0 +1,8 @@
+'''
+Ejemplo for
+'''
+
+print("Ejemplo con for, se digitarán los números del 1 al 9")
+v1 = "123456789"
+for n in v1:
+    print(n)
---
+++ miwhile.py
@@ -0,0 +1,16 @@
+'''
+Ejemplo for
+'''
+
+print("Ejemplo con while, se digitarán los números del 1 al 9")
+c = 1
+while c < 10:
+    print(c)
+    c+=1
+
+
+
+
+
+
+
