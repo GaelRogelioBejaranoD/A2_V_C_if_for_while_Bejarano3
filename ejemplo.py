6bbb925b6c9c77a36980dcd2e560d51145d99f52
Gael Rogelio Bejarano Dominguez <a.22308051281154@cbtis128.edu.mx>
2024-09-06 09:57:18 -0600

    Add files via upload
    If else

---
+++ ejemplo.py
@@ -0,0 +1,28 @@
+'''
+Trabajazo de ifs
+'''
+edad = 17
+
+if edad >= 18:
+    print("Puedes entrar a bailar, lolazo")
+else:
+    print("Vete a dormir sssss...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
