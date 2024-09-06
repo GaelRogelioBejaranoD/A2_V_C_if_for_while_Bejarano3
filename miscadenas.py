8282651acf0c3dc5fbb571201f02d7bb3244ea5f
Gael Rogelio Bejarano Dominguez <a.22308051281154@cbtis128.edu.mx>
2024-09-06 09:35:28 -0600

    Add files via upload
    Manejo de cadenas en Code con Python

---
+++ miscadenas.py
@@ -0,0 +1,28 @@
+'''Rogelio Bejarano
+Trabajando con cadenas
+'''
+Nombre = "Roger"
+print(Nombre[0])
+print(Nombre)
+
+MisNovias=["Press banca","Jalon al pecho con agarre supino cerrado","Elevaciones laterales"]
+for baddies in MisNovias:
+    print(baddies)
+
+print("Otro ejemplo")
+
+b = "Rogelio Bejarano"
+print(b[2:5])
+
+print("a mayusculas")
+a = "Super idol"
+print(a.upper())
+
+print("Concatenar")
+a = "Mike"
+b = "Hawk"
+c = a +" "+ b
+print(c)
+
+
+
---
+++ mivariables.py
@@ -0,0 +1,27 @@
+import math
+x = math.pi
+print(x)
+
+edad = 17 #El 17 se guardara en eda
+nombre = "Rogelio Bejarano"
+
+print("Tu nombre es: ",nombre)
+print("Tu edad es: ",edad)
+
+print("--Tipos de variables--")
+
+x = str(7)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(7.7)
+
+print(x," ",y," ",z);
+print("--Funciones matematicas--")
+
+import math
+x = math.sqrt(37.21)
+print(x)
+
+
+
+
+
